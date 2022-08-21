c3e6ac8f2020f4f1efdeb12d9b8d5e3a1aa673f2
Barnik Maity <maitybarnik@gmail.com>
2022-08-21 12:51:32 +0530

    Inittial commit

---
+++ controllers/main.js
@@ -0,0 +1,24 @@
+const jwt= require('jsonwebtoken')
+const {BadRequestError}=require('../errors')
+
+const login=async(req,res)=>{
+    const {username,password}=req.body
+  if(!username || !password)
+  {
+    throw new BadRequestError('Please enter a username and password')
+  }
+
+  const id=new Date().getDate()
+
+  const token=jwt.sign({id,username},process.env.JWT_SECRET,{expiresIn:'30d'})
+     res.status(200).json({msg:'User created',token})
+}
+
+const dashboard=async(req,res)=>{
+    console.log(req.user)
+    const luckyNumber=Math.floor(Math.random()*100)
+    res.status(200).json({msg:`Hello , ${req.user.username}`,secret:`Your lucky number is ${luckyNumber}`,})
+    
+}
+
+module.exports={login,dashboard}
---
+++ errors/index.js
@@ -0,0 +1,7 @@
+const CustomAPIError=require('./custom-error')
+const BadRequestError = require('./bad-request')
+const UnauthenticatedError = require('./unauthenciated')
+
+module.exports={
+    CustomAPIError,BadRequestError,UnauthenticatedError
+}
